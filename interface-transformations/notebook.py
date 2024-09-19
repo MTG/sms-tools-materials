fbb59fc890d01a370a0bb6cb21728d6f78521425
Alastair Porter <alastair.porter@upf.edu>
2024-09-19 12:15:14 +0200

    Remove py2 tkinter import

--- interface-transformations/notebook.py
+++ interface-transformations/notebook.py
@@ -1,9 +1,4 @@
-try:
-    # for Python2
-    from Tkinter import *  # notice capitalized T in Tkinter
-except ImportError:
-    # for Python3
-    from tkinter import *  # notice lowercase 't' in tkinter here
+from tkinter import *
 
 
 class notebook(object):
